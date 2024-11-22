85fbfd56b73e9b7b2eda4f733d5211fa3641de9b
unknown <ayodhyaweerabahu@gmail.com>
2024-11-22 10:49:22 +0530

    Auto-committed changes

--- Calulator.java
+++ Calulator.java
@@ -1,25 +1,71 @@
 import java.awt.Font;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 class MyCalculator extends JFrame { // MyCalculator extends JFrame
 
-    JButton button;
+    JButton button1;
+    JButton button2;
+    JButton button3;
+    JButton button4;
+    JLabel label;
 
     MyCalculator() { // Constructor of MyCalculator class to set up the layout of the frame
         setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Calculator");
         setLocationRelativeTo(null);
+        setLayout(new GridLayout(4,4));
+
+        JButton[] buttonArr = new JButton[16];
+
+        String[] buttons = {"7", "8", "9", "*", "4", "5", "6", "/", "1", "2", "3", "+", "C", "0", "=", "-"};
+
+        for(int i = 0; i<buttons.length;i++){
+            buttonArr[i]=new JButton(buttons[i]);
+            buttonArr[i].setFont(new Font(getName(), Font.BOLD, 20));
+            add(buttonArr[i]);
+
+        }
+
+
+
+
 
         // Set a layout manager
-        setLayout(null); // Use null for absolute positioning
 
-        button = new JButton();
-        button.setText("Click me");
-        button.setFont(new Font("Arial", Font.BOLD, 20));
-        button.setBounds(150, 150, 100, 50); // x, y, width, height
+        // button1 = new JButton();
+        // button1.setText("Click me");
+        // button1.setFont(new Font("Arial", Font.BOLD, 20));
+        // button1.setBounds(150, 150, 100, 50); // x, y, width, height
 
-        add(button); // Add the button to the frame
+        // button2 = new JButton();
+        // button2.setText("Click me");
+        // button2.setFont(new Font("Arial", Font.BOLD, 20));
+        // button2.setBounds(150, 150, 100, 50); // x, y, width, height
+
+        // button3 = new JButton();
+        // button3.setText("Click me");
+        // button3.setFont(new Font("Arial", Font.BOLD, 20));
+        // button3.setBounds(150, 150, 100, 50); // x, y, width, height
+
+        // button4 = new JButton();
+        // button4.setText("Click me");
+        // button4.setFont(new Font("Arial", Font.BOLD, 20));
+        // button4.setBounds(150, 150, 100, 50); // x, y, width, height
+
+
+        // label = new JLabel();
+        // label.setText("Hello World");
+        // label.setFont(new Font("Arial", Font.BOLD, 20));
+        // label.setBounds(150, 100, 100, 50); // x, y, width, height
+
+
+        // add(button1); // Add the button to the frame
+        // add(button2); // Add the button to the frame
+        // add(button3); // Add the button to the frame
+        // add(button4); // Add the button to the frame
 
         setVisible(true); // Make the frame visible after adding components
     }
