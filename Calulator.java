13c0076a0b225a50ca74fb3aa2fa528f34b12a8a
unknown <ayodhyaweerabahu@gmail.com>
2024-11-22 10:12:25 +0530

    Auto-committed changes

--- Calulator.java
+++ Calulator.java
@@ -1,32 +1,32 @@
 import java.awt.Font;
-
 import javax.swing.*;
 
+class MyCalculator extends JFrame { // MyCalculator extends JFrame
 
-class MyCalculator extends JFrame{ //mycalculator is Jframe now
+    JButton button;
 
-  MyCalculator(){ //constructor of MyCalculator class assign the layout of the frame
-    setSize(400, 400);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setTitle("Calculator");
-    setVisible(true);
-    setLocationRelativeTo(null);
-  }
+    MyCalculator() { // Constructor of MyCalculator class to set up the layout of the frame
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Calculator");
+        setLocationRelativeTo(null);
 
+        // Set a layout manager
+        setLayout(null); // Use null for absolute positioning
+
+        button = new JButton();
+        button.setText("Click me");
+        button.setFont(new Font("Arial", Font.BOLD, 20));
+        button.setBounds(150, 150, 100, 50); // x, y, width, height
+
+        add(button); // Add the button to the frame
+
+        setVisible(true); // Make the frame visible after adding components
+    }
 }
 
-class Calulator{
-  public static void main(String[] args){
-
-    new MyCalculator();//creating object of MyCalculator class
-
-
-
-
-
-;
-
-  }
+class Calculator {
+    public static void main(String[] args) {
+        new MyCalculator(); // Create an object of MyCalculator class
+    }
 }
-
-
